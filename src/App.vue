ea6927b7bdc9d39291905b804a7668c8651b979d
jiriberri <julianiriberri@gmail.com>
2024-04-10 10:25:02 -0300

    adding images and first changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    
+  <div id="app">
+    <div class="search-bar">
+      <input type="text" class="search" placeholder="Search">
+    </div>
   </div>
 </template>
 
@@ -8,9 +10,24 @@
 
 export default {
   name: 'App',
+  data () {
+    return {
+      api_key: '642f37328cb1fc0a2c281c1289fe2e51'
+    } 
+  }
 }
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  body {
+    font-family: 'roboto', sans-serif;
+  }
+
+  
 </style>
